a94f79ed241035690cda2f3c5e4a0c4d1d6f3353
manshawar <825750768@qq.com>
2025-01-15 09:25:16 +0800

    优化图片显示

--- apps/ant-design-pro/src/pages/upload/index.tsx
+++ apps/ant-design-pro/src/pages/upload/index.tsx
@@ -1,11 +1,6 @@
-import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
-import { Card, theme, message, Upload, Image, Table } from 'antd';
+import { theme, Splitter, Image, Flex, message } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { InboxOutlined } from '@ant-design/icons';
-import type { UploadProps, TableColumnsType } from 'antd';
-import { IKContext, IKUpload, IKImage } from 'imagekitio-react';
-import { listen } from '@tauri-apps/api/event';
 import { invoke } from '@tauri-apps/api/core';
 type DownloadStarted = {
   url: string;
@@ -17,34 +12,13 @@ const Index: React.FC = () => {
   const { token } = theme.useToken();
   const { initialState } = useModel('@@initialState');
   const { defaultStyle } = useModel('theme');
-
-  const { Dragger } = Upload;
-
-  const props: UploadProps = {
-    name: 'file',
-    multiple: true,
-    action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-    onChange(info) {
-      const { status } = info.file;
-      if (status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-      if (status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully.`);
-      } else if (status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-    onDrop(e) {
-      console.log('Dropped files', e.dataTransfer.files);
-    },
-  };
+  const [messageApi, contextHolder] = message.useMessage();
+  interface DataType {
+    address: string;
+    key: string;
+  }
   const [dataSource, setDataSource] = useState<DataType[]>([]);
-
-  const publicKey = 'public_f0lEErwJhjlt4SJMaHwZjt0aJ+U=';
-  const urlEndpoint = 'https://ik.imagekit.io/Manshawar';
   const [imageUrl, setImageUrl] = useState<string>('');
-
   async function getPic(event: any) {
     // console.log(event);
     if (event.ctrlKey && event.key === 'v') {
@@ -67,68 +41,55 @@ const Index: React.FC = () => {
       }
     }
   }
+
   const to_upload = async (uint8Array: any) => {
     if (uint8Array) {
       invoke('test', { msg: uint8Array }).then((res) => {
         setDataSource([...dataSource, { address: res as string, key: res as string }]);
         navigator.clipboard.writeText(res as string);
         setImageUrl('');
+        messageApi.open({
+          type: 'success',
+          content: '上传成功',
+        });
       });
     }
   };
 
-  interface DataType {
-    address: string;
-    key: React.Key;
-  }
+  const rightClick = (e: React.MouseEvent<HTMLImageElement, MouseEvent>, src: string) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(src as string);
+    messageApi.open({
+      type: 'success',
+      content: '已复制',
+    });
+  };
 
-  const columns: TableColumnsType<DataType> = [
-    {
-      title: '图片',
-      dataIndex: 'address',
-      key: 'address',
-      align: 'center',
-      render: (value, record, index) => {
-        return (
-          <div
-            style={{ width: '100%' }}
-            onClick={() => {
-              navigator.clipboard.writeText(value);
-            }}
-          >
-            <Image
-              width={200}
-              src={value}
-              placeholder={<Image preview={false} src="/bg/LonelyCAT.png" width={200} />}
-            />
-          </div>
-        );
-      },
-    },
-  ];
   return (
-    <div
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-        display: 'flex',
-
-        justifyContent: 'space-between',
-      }}
-    >
-      <div tabIndex={0} onKeyDown={getPic} style={{ flex: 1 }}>
-        <h2>剪切板中的图片：</h2>
-        {imageUrl && (
-          <img src={imageUrl} alt="剪切板图片" style={{ maxWidth: '100%', height: 'auto' }} />
-        )}
-      </div>
-      <Table
-        pagination={false}
-        dataSource={dataSource}
-        columns={columns}
-        scroll={{ x: 'max-content', y: '300px' }}
-      ></Table>
-    </div>
+    <Splitter style={{ boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', flex: 1 }} layout="vertical">
+      <Splitter.Panel collapsible min="20%">
+        <div tabIndex={0} style={{ height: '100%' }} onKeyDown={getPic}>
+          <h2 style={{ textAlign: 'center' }}>剪切板中的图片</h2>
+          {imageUrl && (
+            <img src={imageUrl} alt="剪切板图片" style={{ maxWidth: '100%', height: 'auto' }} />
+          )}
+          {contextHolder}
+        </div>
+      </Splitter.Panel>
+      <Splitter.Panel collapsible>
+        <Flex wrap gap="small">
+          {dataSource.map((item) => (
+            <Image
+              src={item.address}
+              width={100}
+              height={100}
+              key={item.key}
+              onContextMenu={(e) => rightClick(e, item.address)}
+            />
+          ))}
+        </Flex>
+      </Splitter.Panel>
+    </Splitter>
   );
 };
 
