e6c57ac41d0cc99b26f2655667b4fb9c7c12cfa2
Manshawar <825750768@qq.com>
2025-02-22 20:36:47 +0800

    feat: btn

--- apps/rspress/src/page/css/instance/btn.tsx
+++ apps/rspress/src/page/css/instance/btn.tsx
@@ -1,105 +1,7 @@
-/*
- * @Author: Manshawar 825750768@qq.com
- * @Date: 2025-02-21 15:01:00
- * @LastEditors: Manshawar 825750768@qq.com
- * @LastEditTime: 2025-02-21 17:25:27
- * @FilePath: \Manshawar-cyber\apps\rspress\src\page\css\instance\btn.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import React, { useEffect, useState } from "react";
-import styled, { css } from "styled-components";
-import styles from "./style/btn.module.css";
-import Loading from "./svg/loading.svg";
+import styled, { keyframes, css } from "styled-components";
+import styles from "./style/btn.module.scss";
 
-const Container = styled.div`
-  * {
-    border: 0;
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-
-  --hue: 240;
-
-  --textBg: hsl(var(--hue), 90%, 55%);
-
-  background-color: var(--bg);
-  display: grid;
-  grid-template-columns: repeat(2, auto);
-
-  grid-gap: 2em;
-
-  justify-content: start;
-
-  align-items: center;
-
-  padding: 1em;
-`;
-
-const Button = styled.button`
-  background-color: transparent;
-  border-radius: 1em;
-  display: block;
-  position: relative;
-  width: 5em;
-  height: 2em;
-  transition: width 0.3s ease-in-out;
-  font-size: 0.64em;
-
-  &:not(:disabled):active {
-    transform: translateY(0.1em);
-  }
-`;
-
-const ButtonText = styled.span`
-  background-color: var(--textBg);
-  border-radius: inherit;
-  color: hsl(0, 0%, 100%);
-  display: inline-block;
-  transition:
-    background-color 0.15s linear,
-    color 0.15s 0.3s ease-in-out;
-  width: 100%;
-  height: 100%;
-  display: grid;
-  font-size: 0.8em;
-  place-items: center;
-`;
-
-const ProgressSVG = styled.svg`
-  overflow: visible;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 2em;
-  height: 2em;
-  /* visibility: hidden; */
-`;
-
-const ProgressTrack = styled.circle`
-  r: 12px;
-  stroke: var(--bg2);
-  stroke-width: 24;
-`;
-
-const ProgressFill = styled.circle`
-  stroke: var(--primary1);
-  stroke-dashoffset: 125.66;
-`;
-
-const ProgressCheck = styled.polyline`
-  stroke: hsl(0, 0%, 100%);
-  stroke-dashoffset: 34;
-`;
-let ProgressSvg = () => {
-  return (
-    <ProgressSVG viewBox="0 0 48 48" className={styles["btn__progress"]}>
-      <ProgressTrack className={styles["btn__progress-track"]} r="20" cx="24" cy="24" />
-      <ProgressFill className={styles["btn__progress-fill"]} r="20" cx="24" cy="24" />
-      <ProgressCheck className={styles["btn__progress-check"]} points="12,24 20,32 36,16" />
-    </ProgressSVG>
-  );
-};
 export default function Btn() {
   let [fontSize, setFontSize] = useState(16);
   let [activeName, setActiveName] = useState("");
@@ -131,28 +33,72 @@ export default function Btn() {
     buttonDom.disabled = true;
     clearTimeout(doneTimeout as unknown as NodeJS.Timeout);
     clearTimeout(resetTimeout as unknown as NodeJS.Timeout);
-    setActiveName("btn--loading");
-    console.log("loading.....");
-    doneTimeout = setTimeout(() => {
-      console.log("loading finish.....");
-      console.log("reset.....");
+    setActiveName("btn--running");
 
+    doneTimeout = setTimeout(() => {
       setActiveName("btn--done");
       resetTimeout = setTimeout(() => {
         buttonDom.disabled = false;
-        console.log("reset finish.....");
+
         setActiveName("");
       }, resetDuration);
     }, 600 + submitDuration);
   };
-
+  function mouseDown(e: React.MouseEvent<HTMLButtonElement>) {
+    let dom = e.currentTarget;
+    const { layerX, layerY } = e.nativeEvent;
+    const { width, height } = dom.getBoundingClientRect();
+    console.log(width, height, layerX, layerY);
+  }
+  // <Progress />;
   return (
-    <Container style={{ fontSize: `${fontSize}px` }}>
-      <Button className={`btn ${styles[activeName]}`} onClick={e => handler(e)}>
-        <ButtonText className={styles["btn__text"]}>Send</ButtonText>
-        <ProgressSvg></ProgressSvg>
-      </Button>
-      <ProgressSvg></ProgressSvg>
-    </Container>
+    <div className="flex w-[100%] h-20">
+      <div
+        style={{ fontSize: `${fontSize}px` }}
+        className={`flex justify-center items-center ${styles.container}`}
+      >
+        <button
+          className={[styles.btn, styles[activeName]].join(" ")}
+          onClick={handler}
+          onMouseDown={mouseDown}
+        >
+          <span className={styles["btn__text"]}>Send</span>
+          <svg className={styles["btn__progress"]} viewBox="0 0 48 48" width="48px" height="48px">
+            <circle
+              className={styles["btn__progress-track"]}
+              r="20"
+              cx="24"
+              cy="24"
+              fill="none"
+              stroke="#c7cad1"
+              stroke-width="8"
+            />
+            <circle
+              className={styles["btn__progress-fill"]}
+              r="20"
+              cx="24"
+              cy="24"
+              fill="none"
+              stroke="#000000"
+              stroke-width="8"
+              transform="rotate(-90,24,24)"
+              stroke-dasharray="125.66 125.66"
+              stroke-dashoffset="125.66"
+            />
+            <polyline
+              className={styles["btn__progress-check"]}
+              points="12,24 20,32 36,16"
+              fill="none"
+              stroke="#fff"
+              stroke-width="4"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-dasharray="34 34"
+              stroke-dashoffset="34"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
   );
 }
